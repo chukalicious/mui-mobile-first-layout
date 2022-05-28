89a0573dbbd8d7e8bf85bb47687f4c8c68627872
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-27 20:54:21 -0700

    added basic menu component and imported it to the navbar, displaying in row

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,14 @@ import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import NavMenu from "./NavMenu";
 
 const Navbar = () => {
   return (
     <AppBar>
       <Toolbar>
-        <Typography>appbar</Typography>
+        <Typography>App Title</Typography>
+        <NavMenu />
       </Toolbar>
     </AppBar>
   );
